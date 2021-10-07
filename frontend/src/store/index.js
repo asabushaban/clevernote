01f5c2f06e593ed5f4607e05c01e313c7e97ada9
AJ Abushaban <amjad.abushaban@yahoo.com>
2021-10-07 13:19:47 -0500

    frontend setup completed

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) => {
+const configureStore = preloadedState => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
