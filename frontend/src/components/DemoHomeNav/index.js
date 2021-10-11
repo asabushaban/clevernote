a307b6187fe13331453712f8b450aee75ed1933f
AJ Abushaban <amjad.abushaban@yahoo.com>
2021-10-11 07:36:57 -0500

    renamed repo - refactor DemoHomeNav

--- frontend/src/components/DemoHomeNav/index.js
+++ frontend/src/components/DemoHomeNav/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import Navigation from "../Navigation";
-import { useHistory, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function DemoHomeNav() {
   return (
